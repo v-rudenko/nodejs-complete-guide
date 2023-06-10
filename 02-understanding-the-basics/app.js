c2f9ff95ab55f587796ce9fccc8ad7bbe921addc
Viktor Rudenko <darthvitek@gmail.com>
2023-06-10 18:48:42 +0300

    33. Redirecting Requests

--- 02-understanding-the-basics/app.js
+++ 02-understanding-the-basics/app.js
@@ -1,14 +1,22 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  // process.exit();
+  const url = req.url;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>")
+    res.write("<head><title>Enter Message</title></head>")
+    res.write("<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>")
+    res.write("</body>")
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>")
   res.write("<head><title>My First Page</title></head>")
   res.write("<body><h1>Hello from my Node.js Server</h1></body>")
   res.write("</body>")
   res.end();
+
 });
 
 server.listen(3000);
