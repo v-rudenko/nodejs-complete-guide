094875598b36945bcc5d5d997f405f3c176e38d3
Viktor <v.rudenko@triacom.ua>
2023-06-13 14:55:40 +0300

    50. Logical Errors

--- 04-workflow-and-debugging/01-understanding-npm-scripts/routes.js
+++ 04-workflow-and-debugging/01-understanding-npm-scripts/routes.js
@@ -11,6 +11,7 @@ const requestHandler = (req, res) => {
     );
     res.write('</html>');
     return res.end();
+    // res.end();
   }
   if (url === '/message' && method === 'POST') {
     const body = [];
