07d49638d300a571d2bf81c0c820392696467d74
Viktor <v.rudenko@triacom.ua>
2023-06-14 13:28:10 +0300

    Assignment 2: Time to Practice - Express.js

--- 05-working-with-express/app.js
+++ 05-working-with-express/app.js
@@ -2,14 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next(); // Allows the request to continue to the next middleware in line 
+app.use('/', (req, res, next) => {
+  console.log("This always runs");
+  next();
+})
+
+app.use("/add-product",(req, res, next) => {
+  console.log("In another the middleware!");
+  res.send('<h1>The "Add Product" Page</h1>');
 });
 
-app.use((req, res, next) => {
+app.use("/",(req, res, next) => {
   console.log("In another the middleware!");
-  res.send("<h1>Hello from expresss!</h1>");
+  res.send("<h1>Hello from express!</h1>");
 });
 
 app.listen(3300)
