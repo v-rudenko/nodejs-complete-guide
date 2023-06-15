592a51e35b76acae999149132afd8fe890d9f6da
Viktor <v.rudenko@triacom.ua>
2023-06-15 12:43:13 +0300

    64. Parsing Incoming Requests

--- 05-working-with-express/assignment-2/index.js
+++ 05-working-with-express/assignment-2/index.js
@@ -2,10 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.use("/users", (req, res, next) => {
-  return res.send("<h1>Users List:</h1><ul><li>Roman Shukhevych</li><li>Vasyl Ivakhiv</li><li>Dmytro Klyachkivsky</li><li>Vasyl Kuk</li></ul>");
-});
-
 // app.use("/", (req, res, next) => {
 //   console.log("First function!");
 //   next();
@@ -16,11 +12,14 @@ app.use("/users", (req, res, next) => {
 //   next();
 // });
 
+app.use("/users", (req, res, next) => {
+  return res.send(
+    "<h1>Users List:</h1><ul><li>Roman Shukhevych</li><li>Vasyl Ivakhiv</li><li>Dmytro Klyachkivsky</li><li>Vasyl Kuk</li></ul>"
+  );
+});
+
 app.use("/", (req, res, next) => {
   return res.send('<h1 style="text-align: center">Welcome to "/" page!</h1>');
 });
-
-
-
 
 app.listen(3000);
