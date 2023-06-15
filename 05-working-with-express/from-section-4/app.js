b4dfb8a5df8f450198483e9766fbed1c2b39b3d7
Viktor <v.rudenko@triacom.ua>
2023-06-15 15:55:04 +0300

    69. Creating HTML Pages

--- 05-working-with-express/from-section-4/app.js
+++ 05-working-with-express/from-section-4/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
