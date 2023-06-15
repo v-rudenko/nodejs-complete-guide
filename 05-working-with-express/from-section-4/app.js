9843ff3026856b7db261aefd3be8f7b08206d222
Viktor <v.rudenko@triacom.ua>
2023-06-15 15:31:39 +0300

    68. Filtering Paths

--- 05-working-with-express/from-section-4/app.js
+++ 05-working-with-express/from-section-4/app.js
@@ -11,8 +11,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-app.use("/", (req, res, next) => {
-  next();
+app.use((req, res, next) => {
+  // res.send("<h1>Page not found</h1>");
+  res.statusCode = 404;
+  res.send("<h1>Page not found</h1>");
+  // res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(3000);
