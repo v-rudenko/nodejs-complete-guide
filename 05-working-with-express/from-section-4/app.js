05ad99a82d701dc94c17fced9de09b7319fba222
Viktor <v.rudenko@triacom.ua>
2023-06-15 15:04:02 +0300

    67. Adding a 404 Error Page

--- 05-working-with-express/from-section-4/app.js
+++ 05-working-with-express/from-section-4/app.js
@@ -1,27 +1,18 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(adminRoutes);
+app.use(shopRoutes);
+
 app.use("/", (req, res, next) => {
   next();
-});
-
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title' /><button type='submit'>Add Product</button></form>"
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body)
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from express!</h1>");
 });
 
 app.listen(3000);
