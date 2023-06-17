4e00b198c20409d5b3540f915d99d49b48b714fb
Viktor Rudenko <darthvitek@gmail.com>
2023-06-17 21:22:00 +0300

    184. Creating Products

--- 12-mongo-db/00-starting-setup/util/database.js
+++ 12-mongo-db/00-starting-setup/util/database.js
@@ -1,15 +1,31 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
+    // "mongodb+srv://student:q1w2e3r4@cluster0.3rbvq8m.mongodb.net/shop?retryWrites=true"
     "mongodb+srv://student:q1w2e3r4@cluster0.3rbvq8m.mongodb.net/"
   )
     .then((client) => {
       console.log("Connected!");
-      callback(client)
+      _db = client.db("shop");
+      callback();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+
