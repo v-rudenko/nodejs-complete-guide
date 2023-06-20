35d049c1cf3f7d1a1eb4db4a2c46bd81b6c3c1c9
Viktor Rudenko <darthvitek@gmail.com>
2023-06-20 18:43:54 +0300

    commit

--- 12-mongo-db/00-starting-setup/models/product.js
+++ 12-mongo-db/00-starting-setup/models/product.js
@@ -10,13 +10,27 @@ class Product {
 
   save() {
     const db = getDb();
-    return db.collection("products")
+    return db
+      .collection("products")
       .insertOne(this)
       .then((result) => console.log(result))
       .catch((err) => console.log(err));
   }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
-
-
 
 module.exports = Product;
