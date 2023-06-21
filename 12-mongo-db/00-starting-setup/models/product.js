dee8277483227e4ffb4b628f0e2b254a7fd4a3fb
Viktor Rudenko <darthvitek@gmail.com>
2023-06-21 20:03:07 +0300

    194. Creating New Users

--- 12-mongo-db/00-starting-setup/models/product.js
+++ 12-mongo-db/00-starting-setup/models/product.js
@@ -7,16 +7,18 @@ class Product {
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = new mongodb.ObjectId(id);
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   save() {
     const db = getDb();
     let dbOp;
     if (this._id) {
-      dbOp = db.collection('products').updateOne({_id: new mongodb.ObjectId(this._id)}, {$set: this});
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
     } else {
-      dbOp = db.collection('products').insertOne(this);
+      dbOp = db.collection("products").insertOne(this);
     }
 
     return db
@@ -54,7 +56,14 @@ class Product {
       .catch((err) => console.log(err));
   }
 
-  static updateProduct
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => console.log("Deleted!"))
+      .catch((err) => console.log(err));
+  }
 }
 
 module.exports = Product;
