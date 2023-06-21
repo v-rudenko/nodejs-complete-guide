bdda937c2cb245e2166251dbdd85f07cc5185af5
Viktor Rudenko <darthvitek@gmail.com>
2023-06-21 20:54:11 +0300

    195. Storing the User in our Database

--- 12-mongo-db/00-starting-setup/models/user.js
+++ 12-mongo-db/00-starting-setup/models/user.js
@@ -1,16 +1,21 @@
-const Sequelize = require('sequelize');
+const { ObjectId } = require("mongodb");
+const { getDb } = require("../util/database");
 
-const sequelize = require('../util/database');
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db.collection("users").findOne({ _id: new ObjectId(userId) });
+  }
+}
 
 module.exports = User;
