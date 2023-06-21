7900e7eff11020c3b0b10813f3b831bddf0c4fac
Viktor Rudenko <darthvitek@gmail.com>
2023-06-21 22:06:16 +0300

    198. Storing Multiple Products in the Cart

--- 12-mongo-db/00-starting-setup/app.js
+++ 12-mongo-db/00-starting-setup/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("649336b3d915c4acf82578c1")
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
--- 12-mongo-db/00-starting-setup/models/user.js
+++ 12-mongo-db/00-starting-setup/models/user.js
@@ -2,14 +2,28 @@ const { ObjectId } = require("mongodb");
 const { getDb } = require("../util/database");
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id;
   }
 
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp._id === product.id;
+    // });
+    const updatedCart = { items: [{ productId: new ObjectId(product._id), quantity: 1 }] };
+    const db = getDb();
+    db.collection("users").updateOne(
+      { _id: new ObjectId(this._id) },
+      { $set: { cart: updatedCart } }
+    );
   }
 
   static findById(userId) {
@@ -18,7 +32,7 @@ class User {
       .collection("users")
       .findOne({ _id: new ObjectId(userId) })
       .then((user) => {
-        return user
+        return user;
       })
       .catch((err) => {
         console.log(err);
