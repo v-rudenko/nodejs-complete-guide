432ee46d2822cc66d06159ab554c8e7991d728ca
Viktor <v.rudenko@triacom.ua>
2024-01-18 15:00:42 +0200

    509. Finishing the REST Routes

--- 31-nodejs-and-typescript/src/index.ts
+++ 31-nodejs-and-typescript/src/index.ts
@@ -1,10 +1,13 @@
-import express, { Express, Request, Response } from "express";
+import express from "express";
+import bodyParser from "body-parser";
 
 import todosRoutes from "./routes/todos.js";
 
 const port = 3000;
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(todosRoutes);
 
